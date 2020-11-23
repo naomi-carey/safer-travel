da238cf2882e48227751ea43476fd850c21cb31a
VietQuoc356 <66951449+VietQuoc356@users.noreply.github.com>
2020-11-23 17:32:47 +0000

    first version of map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Map from "./components/Map"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h3>Safer Travel App</h3>
+      <Map/>
     </div>
   );
 }
