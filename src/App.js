d624afddac8903f85d47d3dad63ca69028692690
VietQuoc356 <66951449+VietQuoc356@users.noreply.github.com>
2020-12-03 15:21:39 +0000

    modal thrid update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import Card from './components/TravelCards/Card'
 import FlightCard from './components/Flights/FlightCard';
-import Map from "./components/Map";
+import Map from "./components/Map/Map";
 import Statistics from "./components/Statistics";
-import Navbar from "./components/Navbar/Navbar";
 
 
 function App() {
