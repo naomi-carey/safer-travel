cdc02b136573638aab6b25a080e57772217d1cfb
Naomi Carey <naomicarey00@gmail.com>
2020-12-03 18:04:24 +0100

    added airportt search destination component also modified app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import React from "react";
-
+import React, { Component } from "react";
 import Map from "./components/Map";
-
 import Statistics from "./components/Statistics";
-
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Airport from "./components/Airport";
+import { DateRangePicker } from "react-dates";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h3>Safer Travel App</h3>
+export default class App extends Component {
+  state = {
+    showFlights: false,
+    flightData: {},
+    cityFrom: {},
+    cityTo: {},
+    startDate: null,
+    endDate: null,
+  };
 
-      <Navbar />
+  componentDidMount() {
+    fetch(
+      "https://api.skypicker.com/flights?flyFrom=PRG&to=STN&dateFrom=18/12/2020&dateTo=28/12/2020&partner=picky&v=3"
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          flightData: data,
+        })
+      );
+  }
 
-      <Map />
+  alertStartDate = () => {
+    console.log(this.state.startDate);
+  };
 
-      <Statistics />
-    </div>
-  );
+  alertEndDate = () => {
+    console.log(this.state.endDate);
+  };
+
+  getCityAutoComplete = (city, originDestination) => {
+    this.setState({
+      [originDestination]: city,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+
+        <Map />
+        <div className="airport-search">
+          <h2 className="from">From: </h2>
+          <Airport
+            selectCity={this.getCityAutoComplete}
+            originDestination="cityFrom"
+          />
+          <h2 className="to">To: </h2>
+          <Airport
+            selectCity={this.getCityAutoComplete}
+            originDestination="cityTo"
+          />
+          <div className="calendar">
+            <DateRangePicker
+              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+              startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+              endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+              onDatesChange={({ startDate, endDate }) =>
+                this.setState({ startDate, endDate })
+              } // PropTypes.func.isRequired,
+              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={(focusedInput) => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+            />
+          </div>
+          {/* <div>
+            <button onClick={this.alertStartDate}>
+              Click Me for Start Date
+            </button>
+            <button onClick={this.alertEndDate}>Click Me for End Date</button>
+          </div> */}
+        </div>
+        <Statistics />
+      </div>
+    );
+  }
 }
-
-export default App;
