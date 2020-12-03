e7470c817ee829e50d6e5dacc408edd9f1761eb7
Dean Leonard <deanleonard26@gmail.com>
2020-12-03 13:45:49 +0000

    Dean Commit 3rd december

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
+import Card from './components/TravelCards/Card'
+import FlightCard from './components/Flights/FlightCard';
 
 function App() {
   return (
     <div className="App">
+     <>
+     <Navbar />
+      <FlightCard />
      
-      <Navbar/>
+
+      </>
     </div>
   );
 }
