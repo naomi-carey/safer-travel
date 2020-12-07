a33f72c90764a9555b33eb990d80722d2fdfaea8
Dean Leonard <deanleonard26@gmail.com>
2020-12-07 18:00:46 +0000

    dean

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import './Banner.css';
 function Banner (){
     return (
     <div className='Banner'>
-        <img src='https://images.unsplash.com/photo-1541669257754-60eaba3ad41d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80' atl='Plane in sky with clouds' />
+        <img className='Banner' src='https://images.unsplash.com/photo-1541669257754-60eaba3ad41d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80' atl='Plane in sky with clouds' />
     </div>
     )
 }
