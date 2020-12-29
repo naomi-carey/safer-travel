1a67277e7d27c0ad162d0ecf65921e7e1abf0d76
Naomi Carey <naomicarey00@gmail.com>
2020-12-29 16:15:52 +0100

    cleaned up ewarning in stats.js and removed double log in navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -21,15 +21,17 @@ const Navbar = (props) => {
   return (
     <nav className="Navbar">
       <div>
-    
-        <img className="NavbarLogo" src="https://safetravel.id/img/ico.jpg" alt="NavbarLogo" />
-
         <img
           className="NavbarLogo"
           src="https://safetravel.id/img/ico.jpg"
-          alt="S with plane logo"
+          alt="NavbarLogo"
         />
 
+        {/* <img
+          className="NavbarLogo"
+          src="https://safetravel.id/img/ico.jpg"
+          alt="S with plane logo"
+        /> */}
       </div>
       <div>
         <ul className={renderClasses()}>
