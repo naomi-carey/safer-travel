18bd8b6b8b4c7f0e156a6198f522507e0493f4aa
Dean Leonard <deanleonard26@gmail.com>
2021-01-04 13:55:47 +0000

    Dean

---
+++ src/components/Banners/StatisticsBanner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './StatisticsBanner.css'
+
+
+export default class StatisticsBanner extends Component {
+    render() {
+        return (
+            <div>
+                <img className='StatisticsBanner' src='images/statisticsbanner.jpg' alt='Statistics Banner' />
+            </div>
+        )
+    }
+}
---
+++ src/components/Banners/TravelBanner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './TravelBanner.css'
+
+export default class TravelBanner extends Component {
+    render() {
+        return (
+            <div>
+                <img className='TravelBanner' src='images/SafeTravels.jpg' alt='Safe Travel Poster Banner' />
+            </div>
+        )
+    }
+}
